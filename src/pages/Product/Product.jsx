ba637dd6808cc99957a04171a650f6eac5dbe776
Alexandr Kotenko <kpshmt@gmail.com>
2024-01-18 22:24:20 +0200

    productItem data json (downl, rev)

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -17,21 +17,52 @@ function Product() {
   return (
     <Section>
       <Container>
-        <div className={styles.buttonHolder}>
+        <header className={styles.buttonHolder}>
           <ButtonBack />
-        </div>
-        <div className={styles.product}>
+        </header>
+        {/* <div className={styles.product}>
           <div className={styles.left}>
             <div className={styles.images}></div>
             <div className={styles.mainImg}>
-              <img src={productItem[selectedImg]} alt={productItem.title} />
+              <img src={productItem[selectedImg]} alt={productItem.name} />
             </div>
           </div>
           <div className={styles.right}>
-            <h2>{productItem.title}</h2>
+            <h2>{productItem.name}</h2>
             <p className={styles.right}>{productItem.desc}</p>
           </div>
-        </div>
+        </div> */}
+        <article className={styles.product}>
+          <figure className={styles.figure}>
+            <img
+              src={productItem[selectedImg]}
+              alt={productItem.name}
+              className={styles.mainImg}
+            />
+            <figcaption className={styles.caption}>
+              Downloads: {productItem.downloads}
+            </figcaption>
+          </figure>
+          <div className={styles.info}>
+            <h1 className={styles.name}>Title: {productItem.name}</h1>
+            <h3 className={styles.author}>Author: {productItem.author}</h3>
+            <p className={styles.rating}>Rating: {productItem.rating}</p>
+            <p className={styles.description}>
+              Description: {productItem.description}
+            </p>
+            <div className={styles.reviews}>
+              Reader reviews:
+              {productItem.reviews.map(review => (
+                <blockquote key={review.id} className={styles.review}>
+                  "{review.text}"
+                  <footer className={styles.reviewer}>
+                    {review.reviewer}, {review.age} years old
+                  </footer>
+                </blockquote>
+              ))}
+            </div>
+          </div>
+        </article>
       </Container>
     </Section>
   );
