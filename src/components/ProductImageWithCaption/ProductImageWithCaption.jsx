3c5e6c7b4ca1504967f09a1b359d809962abe8fc
Alexandr Kotenko <kpshmt@gmail.com>
2024-01-20 03:01:19 +0200

    upd

--- src/components/ProductImageWithCaption/ProductImageWithCaption.jsx
+++ src/components/ProductImageWithCaption/ProductImageWithCaption.jsx
@@ -1,25 +1,6 @@
-// import React from 'react';
-// import styles from './ProductImageWithCaption.module.scss';
-
-// function ProductImageWithCaption({ image, name, downloads }) {
-//   return (
-//     <figure className={styles.figure}>
-//       <img src={image} alt={name} className={styles.mainImg} />
-//       <figcaption className={styles.caption}>
-//         <div className={styles.detailBlock}>
-//           <span className={styles.key}>Downloads: </span>
-//           <span className={styles.value}>{downloads}</span>
-//         </div>
-//       </figcaption>
-//     </figure>
-//   );
-// }
-
-// export default ProductImageWithCaption;
-
 import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css'; // Стили для эффекта размытия
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import styles from './ProductImageWithCaption.module.scss';
 
 function ProductImageWithCaption({ image, name, downloads }) {
@@ -29,7 +10,7 @@ function ProductImageWithCaption({ image, name, downloads }) {
         src={image}
         alt={name}
         className={styles.mainImg}
-        effect="blur" // Эффект при загрузке
+        effect="blur"
       />
       <figcaption className={styles.caption}>
         <div className={styles.detailBlock}>
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,19 +1,146 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import Card from 'components/Card/Card';
+// import styles from './ProductList.module.scss';
+
+// const ProductList = ({ products, type }) => {
+//   return (
+//     <ul className={styles.productList}>
+//       {products.map(item => (
+//         <li className={styles.productListItem} key={item.id}>
+//           <Card item={item} type={type} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// ProductList.propTypes = {
+//   products: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       image: PropTypes.string,
+//       author: PropTypes.string,
+//       name: PropTypes.string,
+//       description: PropTypes.string,
+//       badge: PropTypes.string,
+//       type: PropTypes.string,
+//       rating: PropTypes.string,
+//       downloads: PropTypes.string,
+//       reviews: PropTypes.arrayOf(
+//         PropTypes.shape({
+//           id: PropTypes.string,
+//           text: PropTypes.string,
+//           reviewer: PropTypes.string,
+//           age: PropTypes.number,
+//         })
+//       ),
+//     })
+//   ).isRequired,
+//   type: PropTypes.string.isRequired,
+// };
+
+// export default ProductList;
+
+// import React from 'react';
+// import { motion } from 'framer-motion';
+// import PropTypes from 'prop-types';
+// import Card from 'components/Card/Card';
+// import styles from './ProductList.module.scss';
+
+// const ProductList = React.memo(({ products, type, sortChanged }) => {
+//   const variants = {
+//     hidden: { opacity: 0, y: 50 },
+//     visible: i => ({
+//       opacity: 1,
+//       y: 0,
+//       transition: {
+//         delay: i * 0.1,
+//       },
+//     }),
+//   };
+
+//   return (
+//     <ul className={styles.productList}>
+//       {products.map((item, index) => (
+//         <motion.li
+//           key={`${item.id}-${sortChanged}`} // Обновление ключа для срабатывания анимации
+//           custom={index}
+//           initial="hidden"
+//           animate="visible"
+//           variants={variants}
+//           className={styles.productListItem}
+//         >
+//           <Card item={item} type={type} />
+//         </motion.li>
+//       ))}
+//     </ul>
+//   );
+// });
+
+// ProductList.propTypes = {
+//   products: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       image: PropTypes.string,
+//       author: PropTypes.string,
+//       name: PropTypes.string,
+//       description: PropTypes.string,
+//       badge: PropTypes.string,
+//       type: PropTypes.string,
+//       rating: PropTypes.string,
+//       downloads: PropTypes.string,
+//       reviews: PropTypes.arrayOf(
+//         PropTypes.shape({
+//           id: PropTypes.string,
+//           text: PropTypes.string,
+//           reviewer: PropTypes.string,
+//           age: PropTypes.number,
+//         })
+//       ),
+//     })
+//   ).isRequired,
+//   type: PropTypes.string.isRequired,
+//   sortChanged: PropTypes.bool.isRequired,
+// };
+
+// export default ProductList;
+
 import React from 'react';
+import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
 import Card from 'components/Card/Card';
 import styles from './ProductList.module.scss';
 
-const ProductList = ({ products, type }) => {
+const ProductList = React.memo(({ products, type }) => {
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: i => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.1,
+      },
+    }),
+  };
+
   return (
     <ul className={styles.productList}>
-      {products.map(item => (
-        <li className={styles.productListItem} key={item.id}>
+      {products.map((item, index) => (
+        <motion.li
+          key={item.id}
+          custom={index}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          className={styles.productListItem}
+        >
           <Card item={item} type={type} />
-        </li>
+        </motion.li>
       ))}
     </ul>
   );
-};
+});
 
 ProductList.propTypes = {
   products: PropTypes.arrayOf(
